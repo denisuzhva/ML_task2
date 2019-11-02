718f3688c910bcd89cb3059ee1a40479f6ce80fd
mortarsynth <denis.uzhva@yahoo.com>
2019-11-02 12:40:57 +0300

    reparse sparse

--- Src/DatasetParser.py
+++ Src/DatasetParser.py
@@ -7,6 +7,7 @@ import os
 KINO_NUM = 17770
 USR_NUM = 2649429 
 
+
 if __name__ == '__main__':
 
     dataset_path = '../RawData/training_set/'
@@ -18,7 +19,7 @@ if __name__ == '__main__':
         
         with open(dataset_path + kino_id, 'r') as kino_file:
             if kino_iter % 100 == 0:
-                print('Processing file #' + kino_id + ' ... ')
+                print('Processing file #' + kino_id + '...')
             kino_content = kino_file.readlines()
             kino_content  = kino_content[1:]
             for usr in kino_content:
@@ -33,6 +34,7 @@ if __name__ == '__main__':
     kino_matrix = sp.dok_matrix((USR_NUM, KINO_NUM), dtype=np.int8)
     print('Constructing a DOK matrix ... ')
     for kino_id, usr_id_per_kino in kino_dict.items():
+        print('Processing kino id ' + kino_id + '...')
         for usr_id in usr_id_per_kino:
             kino_matrix[usr_id, kino_id] = kino_dict[kino_id][usr_id]
 
